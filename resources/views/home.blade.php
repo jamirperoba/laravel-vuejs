765fbfc1215735cebec67328e238d43609b57028
jamirperoba <jamirperoba@gmail.com>
2018-08-30 23:11:41 -0300

    novos componentes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<!-- <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+  <div class="container">
+      <div class="row">
+          <div class="col-md-10 col-md-offset-1">
+              <painel titulo="Dashboard">
+                <div class="col-md-4">
+                  <painel titulo="Conteúdo 1">
+                    Teste de conteúdo
+                  </painel>
                 </div>
-            </div>
-        </div>
-    </div>
-</div> -->
-<example-component></example-component>
+                <div class="col-md-4">
+                  <painel titulo="Conteúdo 2">
+                    Teste de conteúdo
+                  </painel>
+                </div>
+                <div class="col-md-4">
+                  <painel titulo="Conteúdo 3">
+                    Teste de conteúdo
+                  </painel>
+                </div>
+
+              </painel>
+          </div>
+      </div>
+  </div>
+{{-- <example-component></example-component> --}}
 @endsection
